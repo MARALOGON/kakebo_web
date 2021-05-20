eaea8c37ec66ac24e1132fd2b564895a4903cda0
Marcos Alonso <marcosalonso@MacBook-Pro-de-Marcos.local>
2021-05-21 00:39:31 +0200

    Primera base de datos con SQLite

--- kakebo/views.py
+++ kakebo/views.py
@@ -1,5 +1,22 @@
 from kakebo import app
+import sqlite3
 
 @app.route('/')
 def index():
-    return "Flask rulando"
+    conexion = sqlite3.connect("movimientos.db")
+    cur = conexion.cursor()
+
+    cur.execute('SELECT * FROM movimientos;')
+
+    claves = cur.description #para crear un diccionario
+
+    filas = cur.fetchall()
+    l = []
+
+    for fila in filas:
+        d = {}
+        for columna in fila:
+    
+    conexion.close()
+
+    return "Consulta realizada"
