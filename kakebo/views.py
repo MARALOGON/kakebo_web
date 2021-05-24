b62d212e8bb20adb5ecd629a57a58e83be88c7bf
Marcos Alonso <marcosalonso@MacBook-Pro-de-Marcos.local>
2021-05-25 00:31:55 +0200

    Seguimos con la tabla de movimientos en flask

--- kakebo/views.py
+++ kakebo/views.py
@@ -1,10 +1,11 @@
 from kakebo import app
-from flask import jsonify #jsonify hace lo mismo que json.dumps
+from flask import render_template, jsonify #jsonify hace lo mismo que json.dumps
 import sqlite3
 
 
 @app.route('/')
 def index():
+
     conexion = sqlite3.connect("movimientos.db")
     cur = conexion.cursor() #Cur es una clase para recorrer la base de datos
 
@@ -14,13 +15,23 @@ def index():
 
     filas = cur.fetchall() #fetchall es un metodo que devuelve todas las filas de la base de datos que hemos traido con execute, por si queremos utilizarlas para una variable por ejemplo
     movimientos = []
-
+    saldo = 0
     for fila in filas:
         d = {}
         for tuplaclave, valor in zip(claves, fila):
             d[tuplaclave[0]] = valor
+        if d['esGasto'] == 0:
+            saldo = saldo + d['cantidad']
+        else:
+              saldo = saldo - d['cantidad']
+        d['saldo'] = saldo
         movimientos.append(d)
     
     conexion.close()
 
-    return jsonify(movimientos)
+    return render_template('movimientos.html', datos = movimientos)
+
+
+@app.route('/nuevo', methods=['GET', 'POST'])
+def nuevo():
+    return render_template('alta.html')
