1b9ee04f9e3743cfcfacfbda5abc14df2227760e
Marcos Alonso <marcosalonso@MacBook-Pro-de-Marcos.local>
2021-05-29 20:25:08 +0200

    version con insercion en base de datos y redirect al formulario

--- kakebo/views.py
+++ kakebo/views.py
@@ -1,33 +1,32 @@
 from kakebo import app
-from flask import render_template, jsonify, request #jsonify hace lo mismo que json.dumps
-import sqlite3
+from flask import jsonify, render_template, request, redirect, url_for  #jsonify hace lo mismo que json.dumps
 from kakebo.forms import MovimientosForm
 
+import sqlite3
 
 @app.route('/')
 def index():
-
     conexion = sqlite3.connect("movimientos.db")
-    cur = conexion.cursor() #Cur es una clase para recorrer la base de datos
+    cur = conexion.cursor()
 
-    cur.execute('SELECT * FROM movimientos;')
+    cur.execute("SELECT * FROM movimientos;")
 
-    claves = cur.description #para crear un diccionario
-
-    filas = cur.fetchall() #fetchall es un metodo que devuelve todas las filas de la base de datos que hemos traido con execute, por si queremos utilizarlas para una variable por ejemplo
+    claves = cur.description
+    filas = cur.fetchall()
     movimientos = []
     saldo = 0
     for fila in filas:
         d = {}
-        for tuplaclave, valor in zip(claves, fila):
-            d[tuplaclave[0]] = valor
+        for tclave, valor in zip(claves, fila):
+            d[tclave[0]] = valor
+            print(d)
         if d['esGasto'] == 0:
             saldo = saldo + d['cantidad']
         else:
-              saldo = saldo - d['cantidad']
+            saldo = saldo - d['cantidad']
         d['saldo'] = saldo
         movimientos.append(d)
-    
+
     conexion.close()
 
     return render_template('movimientos.html', datos = movimientos)
@@ -36,12 +35,44 @@ def index():
 @app.route('/nuevo', methods=['GET', 'POST'])
 def nuevo():
     formulario = MovimientosForm()
+
     if request.method == 'GET':
         return render_template('alta.html', form = formulario)
-    else: 
+    else:
+
         if formulario.validate():
-            pass
-        #1. Insertar el movimiento en la base de datos
-        #2. Redirect a la ruta /
+            conexion = sqlite3.connect("movimientos.db")
+            cur = conexion.cursor()
+
+            #Esta es la forma que utilizamos para poder insertar datos en el formulario utilizando SQLite
+            query = "INSERT INTO movimientos (fecha, concepto, categoria, esGasto, cantidad) VALUES (?, ?, ?, ?, ?)" 
+            #Lo que va entre comillas es una cadena multilinea, que asignada a una variable, Python va a ejecutar
+            try:
+                cur.execute(query, [formulario.fecha.data, formulario.concepto.data, formulario.categoria.data,
+                                formulario.esGasto.data, formulario.cantidad.data])
+
+            except sqlite3.Error as el_error:
+                print("Error en SQL INSERT", el_error)
+                return render_template('alta.html', form=formulario)
+
+
+            #Esto es otra forma de escribir lo mismo que lo de encima (INSERT INTO)
+            """ 
+            query = "INSERT INTO movimientos (fecha, concepto, categoria, esGasto, cantidad) VALUES (:fecha, :concepto, :categoria, :esGasto, :cantidad)"
+            cur.execute(query, {
+                'fecha': formulario.fecha.data, 
+                'concepto': formulario.concepto.data, 
+                'categoria': formulario.categoria.data,
+                'esGasto': formulario.esGasto.data, 
+                'cantidad':formulario.cantidad.data
+            }
+            """
+            #1. Insertar el movimiento en la base de datos
+            conexion.commit() #Siempre que se haga un insert, delete, update, etc. para modificar datos de la database, ha de hacerse un commit para fiarlos, si no no se van a grabar
+            conexion.close()
+
+            return redirect(url_for("index"))
+
+           #2. Redirect a la ruta /
         else:
-           return render_template('alta.html', form = formulario) 
+            return render_template('alta.html', form = formulario)
