ed193266728f95f7daf60b9a3a360ff68e2223f8
Marcos Alonso <marcosalonso@MacBook-Pro-de-Marcos.local>
2021-05-30 07:55:33 +0200

    Refactorizando acceso a DB

--- kakebo/views.py
+++ kakebo/views.py
@@ -1,33 +1,56 @@
 from kakebo import app
-from flask import jsonify, render_template, request, redirect, url_for  #jsonify hace lo mismo que json.dumps
+from flask import jsonify, render_template, request, redirect, url_for, flash
 from kakebo.forms import MovimientosForm
 
 import sqlite3
 
-@app.route('/')
-def index():
+
+#Hacemos una funcion para refactorizar el codigo. Todo lo que se refiere a las consultas (query) de la base de datos irá ahora en esta función (cualquier update que se haga se procesa aqui en esta funcion y lo hemos sacado de def index())
+def consultaSQL(query):
+    #1. Abrimos la conexión
     conexion = sqlite3.connect("movimientos.db")
     cur = conexion.cursor()
 
-    cur.execute("SELECT * FROM movimientos;")
+    #2. Ejecutamos la consulta
+    cur.execute(query)
 
+    #3. Obtenemos los datos de la consulta
     claves = cur.description
     filas = cur.fetchall()
-    movimientos = []
-    saldo = 0
+
+    #4. Procesar los datos para devolver una lista de diccionarios. Un diccionario pr fila
+    resultado = []
     for fila in filas:
         d = {}
         for tclave, valor in zip(claves, fila):
             d[tclave[0]] = valor
-            print(d)
+        resultado.append(d)
+
+    conexion.close()
+    return resultado
+
+def modificaTablaSQL(query, parametros=[]):
+    conexion = sqlite3.connect("movimientos.db")
+    cur = conexion.cursor()
+
+    cur.execute(query, parametros)
+
+    conexion.commit() 
+    conexion.close()
+
+
+@app.route('/')
+def index():
+    movimientos = consultaSQL('SELECT * FROM movimientos order by fecha;')
+    
+    saldo = 0
+    for d in movimientos:
         if d['esGasto'] == 0:
             saldo = saldo + d['cantidad']
         else:
             saldo = saldo - d['cantidad']
         d['saldo'] = saldo
-        movimientos.append(d)
 
-    conexion.close()
 
     return render_template('movimientos.html', datos = movimientos)
 
@@ -41,38 +64,19 @@ def nuevo():
     else:
 
         if formulario.validate():
-            conexion = sqlite3.connect("movimientos.db")
-            cur = conexion.cursor()
-
-            #Esta es la forma que utilizamos para poder insertar datos en el formulario utilizando SQLite
+            
             query = "INSERT INTO movimientos (fecha, concepto, categoria, esGasto, cantidad) VALUES (?, ?, ?, ?, ?)" 
-            #Lo que va entre comillas es una cadena multilinea, que asignada a una variable, Python va a ejecutar
+           
             try:
-                cur.execute(query, [formulario.fecha.data, formulario.concepto.data, formulario.categoria.data,
+               modificaTablaSQL(query, [formulario.fecha.data, formulario.concepto.data, formulario.categoria.data,
                                 formulario.esGasto.data, formulario.cantidad.data])
 
             except sqlite3.Error as el_error:
                 print("Error en SQL INSERT", el_error)
-                return render_template('alta.html', form=formulario)
-
-
-            #Esto es otra forma de escribir lo mismo que lo de encima (INSERT INTO)
-            """ 
-            query = "INSERT INTO movimientos (fecha, concepto, categoria, esGasto, cantidad) VALUES (:fecha, :concepto, :categoria, :esGasto, :cantidad)"
-            cur.execute(query, {
-                'fecha': formulario.fecha.data, 
-                'concepto': formulario.concepto.data, 
-                'categoria': formulario.categoria.data,
-                'esGasto': formulario.esGasto.data, 
-                'cantidad':formulario.cantidad.data
-            }
-            """
-            #1. Insertar el movimiento en la base de datos
-            conexion.commit() #Siempre que se haga un insert, delete, update, etc. para modificar datos de la database, ha de hacerse un commit para fiarlos, si no no se van a grabar
-            conexion.close()
-
+                flash("Se ha producido un error en la base de datos. Pruebe en unos minutos") #Flash es una clase que va a inyectar el mensaje de error en la plantilla (alta.html, base.html, etc.) que le digamos, para que aparezca como mensaje generico cuando se produzca un error 
+                return render_template('alta.html', form = formulario)
+          
             return redirect(url_for("index"))
 
-           #2. Redirect a la ruta /
         else:
             return render_template('alta.html', form = formulario)
